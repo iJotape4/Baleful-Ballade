2d5e3fce048c364f0cf961a42a562a2a096ff1f8
JotaPe <jcamacho249@unab.edu.co>
2022-11-20 18:13:44 -0500

    Letter Pop up

--- Baleful Ballade/Assets/Scripts/Interactables/ConcreteObjects/Letter.cs
+++ Baleful Ballade/Assets/Scripts/Interactables/ConcreteObjects/Letter.cs
@@ -7,21 +7,23 @@ namespace Interactables
     public class Letter : SonoricInteraction
     {
         protected override string eventName => eventSoundPath;
+        [SerializeField] protected GameObject messagePopUp;
 
         // Start is called before the first frame update
         protected override void Start()
         {
             base.Start();
             AddOutlineEvents();
+            if(Application.isPlaying)
+            messagePopUp.SetActive(false);
         }
 
         public override void Interact()
         {
             base.Interact();
             levelValidatorScriptableObject.letterEvent?.Invoke();
-
-            gameObject.SetActive(false);
-           
+            flash.SetActive(false);
+            messagePopUp.SetActive(true);          
         }
 
         // Update is called once per frame
@@ -30,6 +32,9 @@ namespace Interactables
             if(Application.isPlaying && flash!=null)
                 EnableFlashAnimation();
         }
-        
+
+        // Called By X button on UI message
+        public void DestroyLetter()=>
+            gameObject.SetActive(false);
     }
 }
