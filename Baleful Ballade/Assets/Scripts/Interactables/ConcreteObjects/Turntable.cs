c6dbb1d7fc843ebbf72baf8b55d8c66ed49095f7
JotaPe <jcamacho249@unab.edu.co>
2022-11-20 15:00:16 -0500

    Phonograph hint

--- Baleful Ballade/Assets/Scripts/Interactables/ConcreteObjects/Turntable.cs
+++ Baleful Ballade/Assets/Scripts/Interactables/ConcreteObjects/Turntable.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Interactables
@@ -5,6 +6,7 @@ namespace Interactables
     public class Turntable : SonoricInteraction
     {
         protected override string eventName => eventSoundPath;
+        [SerializeField] public bool alreadyTouched;
 
         // Start is called before the first frame update
         protected override void Start()
@@ -12,14 +14,25 @@ namespace Interactables
             base.Start();
             AddOutlineEvents();
             eventSoundPath = levelValidatorScriptableObject.completeMelody;
+            StartCoroutine(HintCounter());
         }
 
         public override void Interact()
         {
             base.Interact();
             levelValidatorScriptableObject.startPuzzleEvent?.Invoke();
+            alreadyTouched = true;
         }
 
+        public IEnumerator HintCounter() 
+        {
+            while (!alreadyTouched)
+            {
+                yield return new WaitForSeconds(15f);
+                EnableFlashAnimation();
+            }
+            yield return null;
+        }
         // Update is called once per frame
 
 
